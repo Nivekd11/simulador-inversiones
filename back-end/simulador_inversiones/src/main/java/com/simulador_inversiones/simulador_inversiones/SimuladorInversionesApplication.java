3b32463265c49fbaffa2ebef947203ed391d31f8
Nivekd11 <kevinsandoval06@gmail.com>
2022-09-06 10:37:54 -0500

    primer commit

---
+++ back-end/simulador_inversiones/src/main/java/com/simulador_inversiones/simulador_inversiones/SimuladorInversionesApplication.java
@@ -0,0 +1,13 @@
+package com.simulador_inversiones.simulador_inversiones;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimuladorInversionesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimuladorInversionesApplication.class, args);
+	}
+
+}
---
+++ back-end/simulador_inversiones/src/test/java/com/simulador_inversiones/simulador_inversiones/SimuladorInversionesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simulador_inversiones.simulador_inversiones;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimuladorInversionesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
